31125ae25faf4ea5dff860536e065bae39e5ce4c
ld-a <48872608+ld-a@users.noreply.github.com>
2019-03-30 23:51:17 +0800

    Organized Level Behaviors

--- src/level_behaviours.py
+++ src/level_behaviours.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 """
-level_behaviours.py: classes for scripts to control level entities including 
+level_behaviours.py: classes for scripts to control level entities including
 guard scripted routines
 """
 
@@ -16,55 +16,53 @@ import random
 # level data
 import resources
 
+
 class Level_Behaviours(object):
 	def __init__(self,parent):
 		self.parent = parent
-		# only add things here that depend on static data
-	
+		self.level_init = {}
+		self.level_update = {}
+		# Set Functions for Each Level ...
+		self.level_init['testbig003'] = self.testbig003_init
+		self.level_update['testbig003'] = self.DoorTest
+
+	# on_levelstart: gets called when level is first loaded
+	def on_levelstart(self,level_id):
+
+		# examples:
+		# You can access static level data loaded in from json
+		# resources.levels[level_id].data['tilemap']['layer_b'] etc.
+
+		# you can access the tilemap via:
+		# t = self.parent.tiledlayers
+		# t.UpdateTileLayer(tile, layer, tileval) # layer: 1: mid, 2: fore: change value in displayed tiles
+
+		#item = self.parent.tiledlayers.items[0]
+		#self.parent.tiledlayers.guards[0].PickupItem(item)
+
+		if self.parent.tiledlayers.level_id in self.level_init:
+			self.level_init[self.parent.tiledlayers.level_id]()
+
+	def on_update(self):
+		# this method gets called every game frame (don't make it do super slow stuff :) )
+		if self.parent.tiledlayers.level_id in self.level_update:
+			self.level_update[self.parent.tiledlayers.level_id]()
+
+	# SPECIFIC LEVEL FUNCTIONS BELOW HERE
+
+	def level1_init(self):
+		self.parent.tiledlayers.guards[0].waypoints = [0]
+		self.parent.tiledlayers.guards[0].current_wp = 0
+
+	def testbig003_init(self):
+		self.parent.tiledlayers.guards[0].waypoints = [141,341,528,391]
+		self.parent.tiledlayers.guards[0].current_wp = 0
+		if self.parent.tiledlayers.level_id == 'testbig003':
+			self.DoorTest()
+
 	# Example method for opening a door if two buttons stood on
 	def DoorTest(self):
 		if self.parent.tiledlayers.buttons[0].state == True and self.parent.tiledlayers.buttons[1].state == True:
 			self.parent.tiledlayers.passages[3].Open()
 		else:
 			self.parent.tiledlayers.passages[3].Close()
-	
-	# on_levelstart: gets called when level is first loaded
-	def on_levelstart(self,level_id):
-		
-		# examples:
-		# You can access static level data loaded in from json
-		# resources.levels[level_id].data['tilemap']['layer_b'] etc.
-		# don't edit static data in resources, what you want for the actual game running is in tilemap:
-		#
-		# you can access the tilemap via:
-		# t = self.parent.tiledlayers
-		#
-		# for example occupancy map is: self.parent.tiledlayers.occlayer
-		#
-		# I will provide a bit more documentation for stuff in the tilemap you can do when implemented
-		# For now:
-		# t.UpdateTileLayer(tile, layer, tileval) # layer: 1: mid, 2: fore: change value in displayed tiles
-		#
-		
-		# some example initial setup: set here at on_levelstart
-		if self.parent.tiledlayers.level_id == 'testbig003':
-			
-			# Setting patrol waypoints for guards
-			self.parent.tiledlayers.guards[0].waypoints = [141,341,528,391]
-			self.parent.tiledlayers.guards[0].current_wp = 0
-			
-			# giving the guard a particular item that is in the map already
-			#item = self.parent.tiledlayers.items[0]
-			#self.parent.tiledlayers.guards[0].PickupItem(item)
-		
-		
-	
-	def on_update(self):
-		
-		# this method gets called every game frame (don't make it do super slow stuff :) )
-		# you can access the tilemap in the same way as in "on_startlevel"
-		
-		# example thing: sets up a door cotrol and assigns initial waypoint to a guard
-		if self.parent.tiledlayers.level_id == 'testbig003':
-			self.DoorTest()
-
